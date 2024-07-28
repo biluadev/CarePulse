1b452a4ecbd3d2e5b921b1c500334aac7f5e2c50
biluadev <biluadevcod@gmail.com>
2024-07-28 12:54:13 +0100

    Add a form!

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import PatientForm from "@/components/ui/forms/PatientForm";
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification | PasskeyModal */}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image 
+          <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
@@ -14,9 +17,27 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
 
-        <PatientForm />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image 
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
